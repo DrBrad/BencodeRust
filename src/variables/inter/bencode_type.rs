6ead8c811d707c23e14c55eac4a5babec137fb95
DrBrad <brad@bradeagle.com>
2024-04-20 16:05:59 -0600

    Bencode Types

--- src/variables/inter/bencode_type.rs
+++ src/variables/inter/bencode_type.rs
@@ -10,45 +10,31 @@ impl BencodeType {
 
     pub fn is_prefix(&self, c: char) -> bool {
         match self {
-            BencodeType::NUMBER => {
-                c == self.prefix()
-            },
-            BencodeType::OBJECT => {
-                c == self.prefix()
-            },
-            BencodeType::ARRAY => {
-                c == self.prefix()
-            },
-            BencodeType::BYTES => {
-                c >= '0' && c <= '9'
-            }
+            BencodeType::BYTES => c >= '0' && c <= '9',
+            _ => c == self.prefix(),
         }
     }
 
     pub fn prefix(&self) -> char {
         match self {
-            BencodeType::NUMBER => 'e',
-            BencodeType::OBJECT => 'i',
+            BencodeType::NUMBER => 'i',
+            BencodeType::OBJECT => 'd',
             BencodeType::ARRAY => 'l',
-            BencodeType::BYTES => 0x00 as char
+            BencodeType::BYTES => '\0',
         }
     }
 
     pub fn suffix(&self) -> char {
         match self {
             BencodeType::NUMBER => 'e',
-            BencodeType::OBJECT => 'e',
-            BencodeType::ARRAY => 'e',
-            BencodeType::BYTES => 0x00 as char
+            _ => 'e',
         }
     }
 
     pub fn delimiter(&self) -> char {
         match self {
-            BencodeType::NUMBER => 0x00 as char,
-            BencodeType::OBJECT => 0x00 as char,
-            BencodeType::ARRAY => 0x00 as char,
-            BencodeType::BYTES => ':'
+            BencodeType::BYTES => ':',
+            _ => '\0',
         }
     }
 }
