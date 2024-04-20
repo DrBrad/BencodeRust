d14a8ceabdb57a22cabc6606147236c63eafb135
DrBrad <brad@bradeagle.com>
2024-04-19 21:14:05 -0600

    String encoding

--- src/variables/encoder.rs
+++ src/variables/encoder.rs
@@ -1,19 +1,23 @@
-pub trait Encoder {
-
-}
-
 
 pub fn encode_string(v: &str) -> Vec<u8> {
     let mut r: Vec<u8> = Vec::new();
     let z = v.as_bytes();
 
+    r.extend_from_slice(z.len().to_string().as_bytes());
+    /*
     for c in z.len().to_string().chars() {
         r.push(c as u8);
     }
+    */
 
     r.push(':' as u8);
 
     r.extend_from_slice(z);
+    r
+}
+
+pub fn encode_number(v: usize) -> Vec<u8> {
+    let mut r: Vec<u8> = Vec::new();
     r
 }
 
