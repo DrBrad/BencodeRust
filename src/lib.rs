b659522eff10ef97db7a56b74290beb40ccfbe41
DrBrad <brad@bradeagle.com>
2024-04-22 12:03:44 -0600

    Changed to library format

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,76 @@
 pub mod variables;
 pub mod utils;
 
+/*
+ADD FUNCTIONALITY FOR BYTES [u8] or Vec<u8> for byte to_bencode and from_bencode...
+*/
+
 #[cfg(test)]
 mod tests {
 
+    use std::collections::HashMap;
+    use std::borrow::Borrow;
+    use crate::variables::to_bencode::ToBencode;
+    use crate::variables::from_bencode::FromBencode;
+
     #[test]
-    fn it_works() {
-        //let result = add(2, 2);
-        //assert_eq!(result, 4);
-        println!("Hello World");
+    fn number() {
+        let original = 100.67;
+        let encoded = original.to_bencode();
+        let decoded = f64::from_bencode(&encoded, &mut 0);
+
+        assert_eq!(original, decoded);
+
+        println!("Bencode Number Encoding & Decoding 100%");
+    }
+
+    #[test]
+    fn bytes() {
+        let original = "blank test".to_string();
+        let encoded = original.to_bencode();
+        let decoded = String::from_bencode(&encoded, &mut 0);
+
+        assert_eq!(original, decoded);
+
+        println!("Bencode Bytes / String Encoding & Decoding 100%");
+    }
+
+    #[test]
+    fn array() {
+        let mut vec = Vec::new();
+        vec.push("number 1");
+        vec.push("num 2");
+        let encoded = vec.to_bencode();
+        let decoded = Vec::<String>::from_bencode(&encoded, &mut 0);
+
+        assert_eq!(vec.len(), decoded.len());
+
+        for i in 0..=decoded.len()-1 {
+            assert_eq!(vec[i], decoded[i]);
+        }
+
+        println!("Bencode Array Encoding & Decoding 100%");
+    }
+
+    #[test]
+    fn object() {
+        let mut dic = HashMap::new();
+        dic.insert("hello", "123123");
+        dic.insert("bloop", "another test");
+        let encoded = dic.to_bencode();
+        let decoded = HashMap::<String, String>::from_bencode(&encoded, &mut 0);
+
+        assert_eq!(dic.len(), decoded.len());
+
+        for key in decoded.keys() {
+            if dic.contains_key(key.as_str()) {
+                assert_eq!(dic.get(key.as_str()), decoded.get(key.as_str()));
+            } else {
+                panic!("Key '{}' does not exist in both maps", key);
+            }
+        }
+
+        println!("Bencode Object Encoding & Decoding 100%");
     }
 }
 
