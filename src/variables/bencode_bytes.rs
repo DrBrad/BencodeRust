8b26a79987b0d1ca545f26a8261fcc160559dd81
DrBrad <brad@bradeagle.com>
2024-04-23 00:56:31 -0600

    Understanding LifeTime Modifiers

--- src/variables/bencode_bytes.rs
+++ src/variables/bencode_bytes.rs
@@ -1,56 +1,45 @@
+use std::str::from_utf8;
+use std::slice::from_raw_parts;
+
 use crate::variables::from_bencode::FromBencode;
 use crate::variables::inter::bencode_type::BencodeType;
 use crate::variables::to_bencode::ToBencode;
 
 #[derive(Debug, Eq, Hash, PartialEq)]
-pub struct BencodeBytes<'a>(pub &'a [u8]);//Vec<u8>
+pub struct BencodeBytes<'a>(pub &'a [u8]);
 
 impl<'a> BencodeBytes<'a> {
 
     const TYPE: BencodeType = BencodeType::BYTES;
 
     pub fn as_string(&self) -> &str {
-        std::str::from_utf8(self.0.clone()).unwrap_or_else(|_| panic!("Failed to parse UTF-8 string"))
+        from_utf8(self.0.clone()).unwrap_or_else(|_| panic!("Failed to parse UTF-8 string"))
     }
-
-    /*
-    fn from_bencode(buf: &'a Vec<u8>, off: &mut usize) -> Self {
-        let bytes = &buf[*off..];
-        *off = buf.len();
-        BencodeBytes(bytes)
-    }
-    */
 }
 
 impl<'a> From<&'a str> for BencodeBytes<'a> {
 
     fn from(value: &'a str) -> Self {
-        BencodeBytes(value.as_bytes())
+        Self(value.as_bytes())
     }
 }
 
 impl<'a> From<String> for BencodeBytes<'a> {
 
     fn from(value: String) -> Self {
-        let bytes = value.as_ptr(); // Get raw pointer to string's buffer
-        let len = value.len(); // Get length of string
-        std::mem::forget(value); // Forget about the string to prevent double-free
+        let bytes = value.as_ptr();
+        let len = value.len();
+        std::mem::forget(value);
 
-        // Safety: We assume that the pointer remains valid for the lifetime 'a
         unsafe {
-            BencodeBytes(std::slice::from_raw_parts(bytes, len))
+            Self(from_raw_parts(bytes, len))
         }
-        //BencodeBytes(value.as_bytes())
     }
 }
 
 impl<'a> FromBencode<'a> for BencodeBytes<'a> {
 
     fn from_bencode(buf: &'a Vec<u8>, off: &mut usize) -> Self {
-        //let bytes = &buf[*off..];
-        //*off = buf.len();
-        //BencodeBytes(bytes)
-
         if BencodeType::type_by_prefix(buf[*off] as char) != Self::TYPE {
             panic!("Buffer is not a bencode bytes / string.");
         }
